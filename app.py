c9bceb68afcb32a6b08450c7c6c640f533cd9ab7
Hackbright Student <user@mini-003.(none)>
2014-02-25 16:17:05 -0800

    Up to Your Mission

--- app.py
+++ app.py
@@ -6,15 +6,39 @@ app.secret_key = "shhhhthisisasecret"
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+# NOTE: index() and Process_login are the same as doing the folowing: GET and POST allow more ways to use same URL
+    # if request.method == 'GET':
+    # (if not specified assumes get)
+    # elif request.method == 'POST':
+
+    if session.get("username"):
+        return "User %s is logged in!"%session['username']
+    else:
+        return render_template("index.html")
 
 @app.route("/", methods=["POST"])
 def process_login():
-    return render_template("login.html")
+    username = request.form.get("username")
+    password = request.form.get("password")
 
-@app.route("/youshouldprobablychangethisurl")
+    username = model.authenticate(username, password)
+    if username != None:
+        flash("User authenticated!")
+        session['username'] = username
+    else:
+        flash("Password incorrect, there may be a ferret stampede in progress!")
+
+    return redirect(url_for("index"))
+
+@app.route("/register")
 def register():
     return render_template("register.html")
+
+@app.route("/clear")
+def session_clear():
+    session.clear()
+    return redirect(url_for("index"))
+
 
 if __name__ == "__main__":
     app.run(debug = True)
