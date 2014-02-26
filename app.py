00793f627419645c9c7e0cfe0776de7ff0c7d5ba
Hackbright Student <user@mini-003.(none)>
2014-02-26 15:35:44 -0800

    Completed All parts except WTF

--- app.py
+++ app.py
@@ -1,68 +1,57 @@
 from flask import Flask, render_template, request, redirect, session, url_for, flash
 import model
+import datetime
 
 app = Flask(__name__)
 
-# TODO What is this? 
+# NOTE What is this? "Salting a hashed password"
+# for decrypting cookie, extra layer of security hash("unicorns" + "sshhhhhhhthisisasecret")
+# without this key, flask won't be ale to set up a session. if change, will kick out existing sessions
 app.secret_key = "shhhhthisisasecret"
 
 @app.route("/")
 def index():
 # Checks if user successfully logged in (to session), then gives update
     if session.get('user_id'):
-# TODO (optional) - add username back in in addition to userid
-        return "Userid %r is logged in!"%session['user_id']
+        flash("Userid: %r, username: %r is already logged in!"%(session['user_id'], session['username']))
+        return render_template("index.html")        
+# NOTE u is unicode vs. ascii, includes japanese characters
+# NOTE would need to create another HTML file for render tmeplate that will take care of the u'user' syntax
     else:
         return render_template("index.html")
 
-# def index():
-#     if session.get("username"):
-#         return "User %s is logged in!"%session['username']
-#     else:
-#         return render_template("index.html")
-
-# NOTE: Why is it even necessary to have these 2 handlers in 2 separate ones? What's the rational for splitting work between functions this way? Couldnt just combine to one? 
+# NOTE: Why is it even necessary to have these 2 handlers in 2 separate ones? What's the rationale for splitting work between functions this way? Couldnt just combine to one? 
 #   1 is for displaying form, other is for processing form (keeping it organized)
 # NOTE: index() and Process_login are the same as doing the folowing: GET and POST allow more ways to use same URL
     # if request.method == 'GET':
     # (if not specified assumes get)
     # elif request.method == 'POST':
 
+# NOTE: why is this request.FORM.get instead of request.args.get GET is args, POST is FORM like in HB web app?
+#       best practice is to use POST for changing data, recommended. not hard rule. 
+#       args and GET. Form can be get and that form puts info in uRL and that's a GET in args. 
+#       if form in POST method then puts in FORM
+#       dead giveaway: POST will not go to URL, GET will show up key value pairs in URL
 
-#!!!!$$$ TODO why is this request.FORM.get instead of request.args.get like in HB web app?
 @app.route("/", methods=["POST"])
 def process_login():
+    model.connect_to_db()
     username = request.form.get("username")
     password = request.form.get("password")
 
-###!!!!!Problem #1 TODO Why does this keep giving me an error that NoneType object has no attribute execute when test works just fine?
 # Checks user authenticated to then create a session
     user_id = model.authenticate(username, password)
     if user_id != None:
         flash("User authenticated!")
         session['user_id'] = user_id
-# TODO PROBLEM #3 Can I add to the session dictionary to include 'username' = username to use in #3?
+        session['username'] = username
+    elif model.get_user_by_name(username) is None:
+        flash("Username does not exist, please register a new account")
+        return redirect(url_for("register"))
     else:
         flash("Password incorrect, there may be a ferret stampede in progress!")
 
     return redirect(url_for("index"))
-
-
-# def process_login():
-#     username = request.form.get("username")
-#     password = request.form.get("password")
-
-#     username = model.authenticate(username, password)
-#     if username != None
-#         flash('message') = "User authenticated!"
-#         session['username'] = username
-#     else:
-#         flash('message') = "Password incorrect, there may be a ferret stampede in progress!"
-
-#     return redirect(url_for("index"))
-
-
-
 
 
 @app.route("/clear")
@@ -79,63 +68,57 @@ def view_user(username):
     owner_id = model.get_user_by_name(username)
     wallposts = model.get_wall_posts(owner_id)
     html = render_template("wall.html", wallposts = wallposts)
+    # print "print in_view user fucntion"
     return html
 
+# Adds wall post to database if author is logged in
+@app.route("/user/<username>", methods=["POST"])
+def post_to_wall(username):
+    model.connect_to_db()
+    owner_id = model.get_user_by_name(username)
+    author_id = session.get('user_id')
+    created_at = datetime.datetime.now()
+    content = request.form.get('content')
+    model.post_wall_posts(owner_id, author_id, created_at, content)
+    return redirect(url_for('view_user', username = username))
+# NOTE username = username **values is a "keyword argument pair", left side maps view_user, right side maps to variable within existinf unction
+#   need this because view_user has argument (username)
+# COOL TRICK: Can pass in dictionary as keyword argument with syntax **dictname if want to map several keyword pair arguments
 
-# ######################################################################
-# #### #2 THIS PART IS NEW
-
-# @app.route("/user/<username>", methods=["POST"])
-# def post_to_wall(username):
-#     model.connect_to_db()
-#     owner_id = model.get_user_by_name(username)
-#     author_id = session.get('user_id')
-#     created_at = #???
-#     content = request.form.get("content")
-#     model.get_wall_posts(owner_id,author_id,created_at,content)
-#     return redirect(url_for("view_user"))
-
-
-
-# ###### #3 Part
-# @app.route("/register")
-# def register():
-#     if session.get('user_id'):
-# # TODO How to use url_for for custom user inputs with view_user /user/<username>? 
-# # TODO need to create function that takes user id and returns username?
-#         return redirect(url_for("view_user"))
-#     else:
-#         return render_template("register.html")
-
+# If go to register and user already logged in, go to wall
+@app.route("/register")
+def register():
+    if session.get('user_id'):
+        username = session.get('username')      
+        return redirect(url_for("view_user", username = username))
+    else:
+        return render_template("register.html")
 
 
 # ###### #4 Part
-# @app.route("/register", methods=["POST"])
-# def create_account():
-#     username = request.form.get("username")
-#     password = request.form.get("password")
-#     password_ver = request.form.get("password_verify")
+@app.route("/register", methods=["POST"])
+def create_account():
+    model.connect_to_db()
 
-#     if session.get('user_id'):
-# # TODO How to use url_for for custom user inputs? 
-# # TODO need to create function that takes user id and returns username?
-#         return redirect(url_for("view_user"))
-#     elif model.get_user_by_name(username): 
-#         flash("This username already exists, Please select another one!")
-#         return redirect(url_for("register"))
-# # TODO (optional) - how to avoid deleting everything when redirect back to register page? 
-# # Verification that passwords match 
-#     elif password != password_ver:
-#         flash("Your passwords do not match")
-#         return redirect(url_for("register"))
-#     else:
-#         model.create_user(username, password)
-#         flash("New user was created")            
-#         return redirect(url_for("index"))
+    username = request.form.get("username")
+    password = request.form.get("password")
+    password_ver = request.form.get("password_verify")
 
-# # QUESTION (optional) does it matter how I structure if, elif, else at highest level or need to nest somehow?)
+# Verification if user already exists 
+    if model.get_user_by_name(username): 
+        flash("This username already exists, Please select another one!")
+# TODO (optional) - how to avoid deleting everything when redirect back to register page? 
+        return redirect(url_for("register"))
+# Verification that passwords match 
 
-
+# TODO (optional) (if unsernae not exist take to register page)
+    elif password != password_ver:
+        flash("Your passwords do not match")
+        return redirect(url_for("register"))
+    else:
+        model.create_user(username, password)
+        flash("New user was created")            
+        return redirect(url_for("index"))
 
 if __name__ == "__main__":
     app.run(debug = True)
--- model.py
+++ model.py
@@ -3,31 +3,18 @@ import sqlite3
 DB = None
 CONN = None
 
-
 def connect_to_db():
     global DB, CONN
     CONN = sqlite3.connect("thewall.db")
     DB = CONN.cursor()
 
-# ADMIN_USER="hackbright"
-# ADMIN_PASSWORD=5980025637247534551
-
-# def authenticate(username, password):
-#     if username == ADMIN_USER and hash(password) == ADMIN_PASSWORD:
-#         return ADMIN_USER
-#     else:
-#         return None
-
-
-###!!!!! Problem #1 TODO Why does this keep giving me an error that NoneType object has no attribute execute when test works just fine?
+# returns userid if username-password combo exist in database
 def authenticate(username, password):
     query = """SELECT id FROM Users WHERE username = ? AND password = ?""" 
     DB.execute(query, (username, password))
     row = DB.fetchone()
     if row:
         return row[0]
-    # if username == ADMIN_USER and hash(password) == ADMIN_PASSWORD:
-    #     return ADMIN_USER
     else:
         return None
 
@@ -36,7 +23,10 @@ def get_user_by_name(username):
     query = """SELECT id FROM Users WHERE username = ?"""
     DB.execute(query, (username,))
     row = DB.fetchone()
-    return row[0]
+    if row:
+        return row[0]
+    else:
+        return None
 
 # get all the wall posts for a userid
 def get_wall_posts(owner_id):
@@ -50,35 +40,15 @@ def get_wall_posts(owner_id):
     return posts_list
 
 
-#################################################################
-#### #2 THIS PART IS NEW
-
-## TODO Need to change both tables of database to autoincrement for primary key, should i and how make different primary key #'s between users and wall_posts?
-## TODO Need to find out how to do datetime automatically
-
-# def post_wall_posts(owner_id, author_id, created_at, content):
-#     query = """INSERT into wall_posts (id, owner_id, author_id, created_at, content) values (NULL, ?, ?, ?, ?)"""
-#     DB.execute(query, (owner_id, author_id, created_at, content))
-#     CONN.commit()
+# This posts to wall and inserts into database
+def post_wall_posts(owner_id, author_id, created_at, content):
+    query = """INSERT into wall_posts (owner_id, author_id, created_at, content) values (?, ?, ?, ?)"""
+    DB.execute(query, (owner_id, author_id, created_at, content))
+    CONN.commit()
 
 
-
-#################
-#### #3 THIS PART IS NEW
-# TODO Can I avoid this by adding to session dictionary username info in the  process_login() handler?
-
-# def get_username(userid):
-#     query = """SELECT username FROM Users Where id = ?"""
-#     DB.execute(query, (userid,))
-#     row = DB.fetchone()
-#     return row[0]
-
-
-
-#################
 #### #4 THIS PART IS NEW
-
-# def create_user(username, password):
-#     query = """INSERT INTO Users (id, username, password) VALUES (NULL, ?, ?)"""
-#     DB.execute(query, (username, password))
-#     CONN.commit()
+def create_user(username, password):
+    query = """INSERT INTO Users (username, password) VALUES (?, ?)"""
+    DB.execute(query, (username, password))
+    CONN.commit()
